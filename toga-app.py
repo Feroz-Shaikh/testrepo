587341eae54764522c8b039152d21d5b2f1e559a
Feroz-Shaikh <feroz.shaikh@ust-global.com>
2018-04-15 12:04:30 +0530

    Introducing google-cloud-publisher

--- toga-app.py
+++ toga-app.py
@@ -1,8 +1,35 @@
 import toga
+import time
+
+# Imports the Google Cloud client library
+from google.cloud import pubsub_v1
+
+# Variables
+project_name = "track-it-now"
+topic_name = "my-topic"
+
+# Instantiates a client
+publisher = pubsub_v1.PublisherClient()
+# The resource path for the new topic contains the project ID and the topic name.
+topic_path = publisher.topic_path(project_name, topic_name)
+
+
+def publish_messages(project_name, topic_name):
+    ''' Publishes multiple messages to a Pub/Sub topic. '''
+
+    for n in range(1, 11):
+        data = u'Message number {}'.format(n)
+        # Data must be a bytestring
+        data = data.encode('utf-8')
+        publisher.publish(topic_path, data=data)
+        print('Published message is: {}'.format(data))
+        time.sleep(3)
 
 def send_telemetrydata(widget):
     # Send telemetry data to Google Cloud
-    print("Sending telemetry data")
+    print("==========>Started sending telemetry data..")
+    publish_messages(project_name, topic_name)
+    print("==========>Successfully sent data.\n")
 
 def build(app):
     main_box = toga.Box()
@@ -16,7 +43,7 @@ def build(app):
     return main_box
 
 def main():
-    return toga.App('Trackersweb', 'com.trackersweb.trackit', startup=build)
+    return toga.App('Trackersweb', 'com.trackersweb', startup=build)
 
 if __name__ == '__main__':
     main().main_loop()
